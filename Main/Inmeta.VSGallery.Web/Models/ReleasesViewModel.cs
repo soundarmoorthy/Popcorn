550967f95eb8e51cb82d1acf8b4e09697b841d60
OsirisJakob_cp <SND\OsirisJakob_cp>
2014-09-15 12:51:18 -0700

    Fixed links so that they work for virtual directories as well

--- Main/Inmeta.VSGallery.Web/Models/ReleasesViewModel.cs
+++ Main/Inmeta.VSGallery.Web/Models/ReleasesViewModel.cs
@@ -1,27 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
-using System.Web;
 using Inmeta.VSGallery.Model;
 
 namespace Inmeta.VSGallery.Web.Models
 {
-    public class ExtensionViewModel
-    {
-        public Extension Extension { get; set; }
-        public string ProjectDescription { get; set; }
-        public StarRating StarRating { get; set; }
-        public string DownloadUrl { get; set; }
-        public ExtensionViewModel(Extension e, string projectDescription, double averageRating, string baseUrl)
-        {
-            Extension = e;
-            ProjectDescription = projectDescription;
-            StarRating = new StarRating(e.Release.Id, averageRating);
-            DownloadUrl = e.DownloadUrl(baseUrl);
-        }
-    }
     public class ReleasesViewModel
     {
         public string SearchText { get; set; }
@@ -40,19 +24,6 @@ namespace Inmeta.VSGallery.Web.Models
         public ReleasesViewModel(IEnumerable<Release> releases)
         {
             this.Releases = releases.Select(e => new ReleaseViewModel(e));
-        }
-    }
-
-    public class UploadExtensionModel
-    {
-        [Required]//, FileExtensions(Extensions = "vsix", ErrorMessage = "Select a VSIX file")]
-        public HttpPostedFileBase File { get; set; }
-
-        public byte[] ReadFileContent()
-        {
-            var content = new byte[this.File.ContentLength];
-            this.File.InputStream.Read(content, 0, content.Length);
-            return content;
         }
     }
 
@@ -89,17 +60,5 @@ namespace Inmeta.VSGallery.Web.Models
 
         public StarRating StarRating { get; set; }
 
-    }
-
-    public class StarRating
-    {
-        public int ReleaseId { get; set; }
-        public int NrStars { get; set; }
-
-        public StarRating(int releaseId, double averageRating)
-        {
-            ReleaseId = releaseId;
-            NrStars = Convert.ToInt32(averageRating);
-        }
     }
 }
